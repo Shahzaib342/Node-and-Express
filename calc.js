12da0d03bf1b478ceda5505cbc53ec48c037ffd0
Shahzaib Ali Hassan <shahzaibalihassan@gmail.com>
2021-06-18 08:57:09 +0500

    Adding new calc end points

--- calc.js
+++ calc.js
@@ -1,5 +1,14 @@
 module.exports = {
     add: function(data, M) {
-     return data.num + M;
+     return  M += data.num;
+    },
+    sub: function(data, M) {
+        return M -= data.num;
+    },
+    multiply: function(data, M) {
+        return M *= data.num;
+    },
+    divide: function(data, M) {
+        return M/data.num;
     }
 };
--- server.js
+++ server.js
@@ -20,6 +20,24 @@ async function post(request, func) {
               });
               M = await promise; 
             break;
+        case "sub":
+            let promise2 = new Promise((resolve, reject) => {
+                setTimeout(() => resolve(calculator.sub(request.body, M)), 1000)
+            });
+            M = await promise2;
+            break;
+        case "multiply":
+            let promise3 = new Promise((resolve, reject) => {
+                setTimeout(() => resolve(calculator.multiply(request.body, M)), 1000)
+            });
+            M = await promise3;
+            break;
+        case "divide":
+            let promise4 = new Promise((resolve, reject) => {
+                setTimeout(() => resolve(calculator.divide(request.body, M)), 1000)
+            });
+            M = await promise4;
+            break;
         }
       return {'M': M};
   }
@@ -33,4 +51,31 @@ app.post('/calc/add', function(request, response) {
      return post(request, 'add').then(function(result){
         response.send(result);
      });
+});
+
+app.post('/calc/sub', function(request, response) {
+    return post(request, 'sub').then(function(result){
+        response.send(result);
+    });
+});
+
+app.post('/calc/multiply', function(request, response) {
+    return post(request, 'multiply').then(function(result){
+        response.send(result);
+    });
+});
+
+app.post('/calc/divide', function(request, response) {
+    return post(request, 'divide').then(function(result){
+        response.send(result);
+    });
+});
+
+app.get('/calc/M', function(request, response) {
+    return response.send( {'M': M});
+});
+
+app.get('/calc/reset', function(request, response) {
+    M = 0 ;
+    return response.send( {'M': M});
 });
