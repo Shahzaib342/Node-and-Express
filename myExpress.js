df32d94375c6eeb43eac87a73ff5f92188814985
shahzaib ali <shahzaib.ali@oneclout.com>
2021-06-18 18:56:23 +0500

    Added html page to use calc.js and endpoint for it to return html

---
+++ myExpress.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const { response } = require('express');
+var app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true }));
+let M;
+
+var server = app.listen(8000, function () {
+    console.log("Server is up!");
+});
+
+app.get('/calcHtml', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
